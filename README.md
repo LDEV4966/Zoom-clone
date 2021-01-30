e7cf555446ccdacbc28c5047c14a2ea44dff97b5
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2021-01-30 21:08:02 +0900

    Initialize and Make room Id

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Zoom clone
- Build a Zoom Clone with Node JS
+
+Build a Zoom Clone with Node JS
+
+## Plan of Action
+
+- Initialize our NodeJS project => DONE
+- Initialize our first view => DONE
+- Create a room id => DONE
+- Add the ability to allow others to stream their video
+- Add styling
+- Add the ability to create message
+- Add Mute button
+- Add Stop Video button
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app); //http module is bulit in js so we dont have to npm install
+const { v4: uuidv4 } = require('uuid');
+app.set("view engine", "ejs"); // 화면 node.js express engine을 ejs로 설정
+
+app.get("/", (req, res) => {
+  //res.status(200).send("Hello world");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId : req.params.room});
+})
+server.listen(3030);
