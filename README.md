224e883f59f3b15b5148d7c10aaf5e5adf59eddc
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2021-02-03 15:10:51 +0900

    Origin

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Zoom clone
 
-Build a Zoom Clone with Node JS
+Build a Zoom Clone with Node JS and Socket
 
 ## Plan of Action
 
-- Initialize our NodeJS project => DONE
-- Initialize our first view => DONE
-- Create a room id => DONE
-- Add the ability to allow others to stream their video => DONE
+- Initialize our NodeJS project
+- Initialize our first view
+- Create a room id
+- Add the ability to allow others to stream their video
 - Add styling
 - Add the ability to create message
 - Add Mute button
 - Add Stop Video button
+
+### inspired by Clever Programer
--- server.js
+++ server.js
@@ -1,34 +1,3 @@
-/*
-const express = require("express");
-const app = express();
-const server = require("http").Server(app); //http module is bulit in js so we dont have to npm install
-const io = require("socket.io")(server);
-const { v4: uuidv4 } = require("uuid");
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
-app.use("/peerjs", peerServer);
-
-app.set("view engine", "ejs"); // 화면 node.js express engine을 ejs로 설정
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  //res.status(200).send("Hello world");
-  res.redirect(`/${uuidv4()}`);
-});
-
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
-});
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
-  });
-});
-server.listen(3030);
-*/
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
